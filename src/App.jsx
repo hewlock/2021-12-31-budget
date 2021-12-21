c0116d9cf92bd9e3c980bc3e5320011a791698e3
Matthew Moulton <matthew.moulton@gmail.com>
2021-12-21 08:21:14 -0500

    #1 wire in redux

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 import {
+    useCallback,
+    useMemo,
+} from 'react';
+import configureStore from './configureStore';
+import {
+    increment,
+    getCount,
+} from './ducks/count';
+import {
+    Provider,
+    useDispatch,
+    useSelector,
+} from 'react-redux';
+import {
     BrowserRouter,
     Link,
     Outlet,
@@ -9,46 +23,63 @@ import {
     Routes,
 } from 'react-router-dom';
 
-export default function App() {
+function Counter() {
+    const count = useSelector(getCount)
+    const dispatch = useDispatch();
+    const handleClick = useCallback((e) => dispatch(increment(1)), []);
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <BrowserRouter>
-                    <Routes>
-                        <Route
-                            path="/"
-                            element={
-                                <>
-                                    <h1>Main</h1>
-                                    <div>
-                                        <Link to="/">home</Link> | <Link to="/about">about</Link>
-                                    </div>
-                                    <Outlet />
-                                </>
-                            }
-                        >
+        <>
+            {count}
+            <button onClick={handleClick}>Increment</button>
+        </>
+    );
+}
+
+export default function App() {
+    const store = useMemo(() => configureStore({}), []);
+
+    return (
+        <Provider store={store}>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <BrowserRouter>
+                        <Routes>
                             <Route
-                                path="/about"
+                                path="/"
                                 element={
-                                    <h2>About</h2>
+                                    <>
+                                        <h1>Main</h1>
+                                        <Counter />
+                                        <div>
+                                            <Link to="/">home</Link> | <Link to="/about">about</Link>
+                                        </div>
+                                        <Outlet />
+                                    </>
                                 }
-                            />
-                        </Route>
-                    </Routes>
-                </BrowserRouter>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+                            >
+                                <Route
+                                    path="/about"
+                                    element={
+                                        <h2>About</h2>
+                                    }
+                                />
+                            </Route>
+                        </Routes>
+                    </BrowserRouter>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        </Provider>
     );
 }
