5508e7e35ecde337436795f7b187d5fb63b39dcc
Matthew Moulton <matthew.moulton@gmail.com>
2021-12-22 06:08:22 -0500

    #1 bootstrap & intl

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { IntlProvider, FormattedMessage } from 'react-intl';
+import en from './lang/en.json'
+
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 import {
     useCallback,
     useMemo,
+    useState,
 } from 'react';
 import configureStore from './configureStore';
 import {
@@ -27,10 +35,31 @@ function Counter() {
     const count = useSelector(getCount)
     const dispatch = useDispatch();
     const handleClick = useCallback((e) => dispatch(increment(1)), []);
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <>
             {count}
-            <button onClick={handleClick}>Increment</button>
+            <Button variant="primary" onClick={handleClick}>
+                Increment
+            </Button>
+            <Button variant="primary" onClick={handleShow}>
+                Show
+            </Button>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton={true}>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </>
     );
 }
@@ -39,47 +68,49 @@ export default function App() {
     const store = useMemo(() => configureStore({}), []);
 
     return (
-        <Provider store={store}>
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <BrowserRouter>
-                        <Routes>
-                            <Route
-                                path="/"
-                                element={
-                                    <>
-                                        <h1>Main</h1>
-                                        <Counter />
-                                        <div>
-                                            <Link to="/">home</Link> | <Link to="/about">about</Link>
-                                        </div>
-                                        <Outlet />
-                                    </>
-                                }
-                            >
+        <IntlProvider messages={en} locale="en" defaultLocale="en">
+            <Provider store={store}>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <p>
+                            <FormattedMessage id="hello" />
+                        </p>
+                        <BrowserRouter>
+                            <Routes>
                                 <Route
-                                    path="/about"
+                                    path="/"
                                     element={
-                                        <h2>About</h2>
+                                        <>
+                                            <h1>Main</h1>
+                                            <Counter />
+                                            <div>
+                                                <Link to="/">home</Link> | <Link to="/about">about</Link>
+                                            </div>
+                                            <Outlet />
+                                        </>
                                     }
-                                />
-                            </Route>
-                        </Routes>
-                    </BrowserRouter>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
-        </Provider>
+                                >
+                                    <Route
+                                        path="/about"
+                                        element={
+                                            <h2>About</h2>
+                                        }
+                                    />
+                                </Route>
+                            </Routes>
+                        </BrowserRouter>
+                        <a
+                            className="App-link"
+                            href="https://reactjs.org"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Learn React
+                        </a>
+                    </header>
+                </div>
+            </Provider>
+        </IntlProvider>
     );
 }
